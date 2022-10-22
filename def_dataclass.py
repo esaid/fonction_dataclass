a46f0a72360747ec31c78b533d736219dd89d46d
esaid <emmanuel.said@cern.ch>
2022-10-22 13:30:39 +0200

    modification nom class par info_values

--- def_dataclass.py
+++ def_dataclass.py
@@ -6,20 +6,21 @@ from dataclasses import dataclass
 
 # parametres
 @dataclass
-class return_values:
+class info_values:
     info: str
     nb: int
+
 
 # ma fonction
 def info(message, indice):
     info = 'voici mon info  ' + message + ' indice: ' + str(indice)
     nb = len(message)
-    t = return_values(info, nb)
+    t = info_values(info, nb)
     return t
+
 
 # utilisation de ma fonction
 myinfo = info('information', 1)
-
 
 # les valeurs
 print('longueur de mon message :', myinfo.nb)
