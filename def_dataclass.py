2ad28f7e3ae81d1f2cd3cb6116761d9647413881
esaid <emmanuel.said@cern.ch>
2022-11-20 15:48:09 +0100

    modification nom class par info_values instantiation field list

--- def_dataclass.py
+++ def_dataclass.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 # parametres
@@ -10,6 +10,7 @@ class info_values:
     message: str
     indice: int
     nb = 0
+    params : list = field(default_factory=lambda : [])
 
     def info_total(self):
         info = 'voici mon info  ' + self.message + ' indice: ' + str(self.indice) + ' longueur:  ' + str(len(self.message))
@@ -19,6 +20,22 @@ class info_values:
 
 # instanciation
 myinfo = info_values('information', 1)
+myinfo.params.append('value')
+myinfo.params.append('scaling_factor')
+myinfo.params.append('offset')
+
+print(myinfo.params)
+print(myinfo.params[0])
+
+
+print(myinfo.params)
+print(myinfo)
+
+print(myinfo)
+
+print(myinfo.params.pop())
+
+
 
 # les valeurs
 print('longueur de mon message :', myinfo.nb)
